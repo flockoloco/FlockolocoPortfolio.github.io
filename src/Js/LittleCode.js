0de210e1219966389572a8a958e586343ac11a2c
Arthur Santos <tutus.6@hotmail.com>
2022-05-14 23:29:51 +0200

    Site

---
+++ src/Js/LittleCode.js
@@ -0,0 +1,72 @@
+
+const scrollProgress = document.getElementById('progressBar');
+const height =
+document.getElementById('Studies').offsetHeight;
+
+//const offSetFooter = document.getElementById('Projects').offsetHeight + document.getElementById('Contacts').offsetHeight + document.getElementById('Footer').offsetHeight;
+
+const firstProject = document.getElementById('first');
+const GameJam2020 = document.getElementById('second')
+const secondProject = document.getElementById('third');
+const thirdProject = document.getElementById('fourth');
+const fourthProject = document.getElementById('fifth');
+const fifthProject = document.getElementById('sixth');
+
+
+firstProject.addEventListener('click', ()=> {
+  document.getElementById('Projects').scrollIntoView();
+})
+GameJam2020.addEventListener('click', ()=> {
+  document.getElementById('Projects').scrollIntoView();
+})
+secondProject.addEventListener('click', ()=> {
+  document.getElementById('Projects').scrollIntoView();
+})
+thirdProject.addEventListener('click', ()=> {
+  document.getElementById('Projects').scrollIntoView();
+})
+fourthProject.addEventListener('click', ()=> {
+  document.getElementById('Projects').scrollIntoView();
+})
+fifthProject.addEventListener('click', ()=> {
+  document.getElementById('Projects').scrollIntoView();
+})
+
+window.addEventListener('scroll', () => {
+  const scrollTop =
+    document.body.scrollTop - ( document.getElementById('NavbarContainer').offsetHeight + document.getElementById('AboutMe').offsetHeight);
+  scrollProgress.style.height = `${((scrollTop + 50) / height) * 100}%`;
+  
+  if(scrollTop < - 100)
+  {
+    scrollProgress.style.height = `${0}%`;
+  }
+});
+
+const AboutMeButton = document.getElementById('AboutButton');
+
+AboutMeButton.addEventListener('click', MakeItVisible);
+
+function MakeItVisible()
+{
+  const textToDissapear = document.getElementById('AboutIntro');
+  const buttonToDissapear = document.getElementById('AboutButton');
+
+  const photoToAppear = document.getElementById('AboutPhoto');
+  const textToAppear = document.getElementById('AboutText');
+
+  textToAppear.style.objectPosition = 'absolute';
+
+  textToAppear.style.visibility = 'visible';
+  photoToAppear.style.visibility = 'visible';
+
+  buttonToDissapear.style.pointerEvents = 'none';
+
+  textToDissapear.classList.add("elementToFadeOut");
+  buttonToDissapear.classList.add('elementToFadeOut');
+
+  photoToAppear.classList.add('elementToFadeIn');
+  textToAppear.classList.add('elementToFadeIn');
+  
+}
+
